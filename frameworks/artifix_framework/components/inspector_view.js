d460e316bd736c13eef4d04378122d48188083fe
Kyle Carriedo <kcarriedo@thecodeboutique.com>
2011-12-22 10:55:38 -0800

    used segmented button for swaping out views

--- frameworks/artifix_framework/components/inspector_view.js
+++ frameworks/artifix_framework/components/inspector_view.js
@@ -12,42 +12,40 @@
 // @extends SC.View
 
 
-ART.InspectorView = SC.PalettePane.extend(
-  /** @scope ART.InspectorView.prototype */{
-  classNames: 'silver_background'.w(),
-      
-  contentView: SC.View.extend({
-    childViews: ["top", "middle"],
-    
-    top: SC.ToolbarView.design({
-      classNames: 'top_bar'.w(),
-      layout: { top: 0, left: 0, right: 0, height: 25 },
-      childViews: ["left", "right"],
-      
-      left: SC.ButtonView.design({
-        classNames: 'blue_button_sml'.w(),
-        layout: { centerY: 0, left: 20, height: 26, width: 82},
-        title: 'Parts',
-        action: 'Parts',
-        target: 'ART.inspectorViewController'
-      }),
-      
-      right: SC.ButtonView.design({
-        classNames: 'blue_button_sml'.w(),
-        layout: { centerY: 0, right: 20, height: 26, width: 82},
-        title: 'Views',
-        action: 'Views',
-        target: 'ART.inspectorViewController'
-      }),
-      
-    }),
-    
-    middle: SC.ContainerView.design({
-			nowShowingBinding: 'ART.inspectorViewController.nowShowing',
-			layout: { left: 0, top: 36, bottom: 0, right: 0 }
-		}),
-     
-  })  
-     
+ART.InspectorView = SC.PalettePane.extend({
 
-});
+      classNames: 'silver_background'.w(),
+
+      contentView: SC.View.extend({
+        childViews: ["top", "middle"],
+
+        top:SC.ToolbarView.design({
+          classNames: 'top_bar'.w(),
+          childViews: ["viewSelection"],
+          layout: { top: 0, left: 0, right: 0, height: 25 },
+
+          viewSelection:SC.SegmentedView.design({
+            layout: { top: 0, left: 0, right: 0, bottom: 0 },
+            items:[
+              {title:"Parts", value:"parts"},
+              {title:"Views", value:"views"}
+            ],
+            itemTitleKey:"title",
+            itemValueKey:"value",
+            value:"parts",
+            didValueChange:function() {
+              if (this.get('value') === "parts") {
+                ART.inspectorViewController.set('nowShowing', 'ART.leftone.leftView');
+              } else {
+                ART.inspectorViewController.set('nowShowing', 'ART.rightone.rightView');
+              }
+            }.observes('value')
+          })
+        }),
+
+        middle: SC.ContainerView.design({
+          nowShowingBinding: 'ART.inspectorViewController.nowShowing',
+          layout: { left: 0, top: 36, bottom: 0, right: 0 }
+        })
+      })
+    });
