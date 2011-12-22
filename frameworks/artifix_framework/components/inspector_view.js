0f55f13ef49fcbe56de833cd06f19d8de75ac2c6
Kyle Carriedo <kcarriedo@thecodeboutique.com>
2011-12-22 11:57:53 -0800

    fixed pane not dragging

--- frameworks/artifix_framework/components/inspector_view.js
+++ frameworks/artifix_framework/components/inspector_view.js
@@ -25,7 +25,7 @@ ART.InspectorView = SC.PalettePane.extend({
           layout: { top: 0, left: 0, right: 0, height: 25 },
 
           viewSelection:SC.SegmentedView.design({
-            layout: { top: 0, left: 0, right: 0, bottom: 0 },
+            layout: { top: 0, left: 30, right: 30, bottom: 0 },
             items:[
               {title:"Parts", value:"parts", action: "Parts", target: "ART.inspectorViewController" },
               {title:"Views", value:"views", action: "Views", target: "ART.inspectorViewController" }
