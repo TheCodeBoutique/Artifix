1618957662ceec98fa8be7b1d255af49e8a1f4aa
Kyle Carriedo <kcarriedo@thecodeboutique.com>
2011-12-26 12:14:46 -0800

    Start of the using delete key to remove objects. Selection and resizing working UI needs clean up

---
+++ apps/artifix/controllers/parts_controller.js
@@ -0,0 +1,15 @@
+// ==========================================================================
+// Project:   Artifix.partsController
+// Copyright: @2011 My Company, Inc.
+// ==========================================================================
+/*globals Artifix */
+
+/** @class
+
+    (Document Your Controller Here)
+
+ @extends SC.ArrayController
+ */
+Artifix.partsController = SC.ArrayController.create( {
+
+ });
---
+++ apps/artifix/tests/controllers/parts_test.js
@@ -0,0 +1,15 @@
+// ==========================================================================
+// Project:   Artifix.partsController Unit Test
+// Copyright: @2011 My Company, Inc.
+// ==========================================================================
+/*globals Artifix module test ok equals same stop start */
+
+module("Artifix.partsController");
+
+// TODO: Replace with real unit test for Artifix.partsController
+test("test description", function() {
+  var expected = "test";
+  var result   = "test";
+  equals(result, expected, "test should equal test");
+});
+
